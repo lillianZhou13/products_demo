5bd546f9a20983671039b1eb48cfb8a3f63059b3
bian zhou <bianzhou23@gmail.com>
2021-10-13 21:45:37 +1100

    header style
    header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {NavLink} from 'react-router-dom';
+import {NavLink,Link} from 'react-router-dom';
 
 const Header = ({cartLength}) => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div className="container">
-                <NavLink className="navbar-brand" to="/">Ecommerce</NavLink>
-                <div>
-                    <ul className="navbar-nav ml-auto">
+        <nav className="navbar navbar-expand-lg navbar-light justify-content-between" id="header">
+           
+            <Link className="navbar-brand" to="/products" >
+              <h1>Fulfill<span>ant</span></h1>
+            </Link>
+           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation" >
+            <span className="navbar-toggler-icon"></span>
+           </button>
+                
+            <div className="collapse navbar-collapse" id="navbar">
+                <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                       <NavLink className="nav-link" to ={"/signup"}>Sign Up</NavLink>
+                       <NavLink className="nav-link" to ={"/signup"}>
+                        <i className="fa fa-sign-in mr-2"  />
+                           Sign Up</NavLink>
                     </li>
-                     <li className="nav-item">
-                            <NavLink className="nav-link" to={"/cart"}>
-                                <i className="fa fa-shopping-cart mr-2" aria-hidden="true" />
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to={"/cart"}>
+                            <i className="fa fa-shopping-cart mr-2" />
                                 Cart {cartLength ? `(${cartLength})`: ''}
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
+                        </NavLink>
+                     </li>
+                 </ul>
             </div>
+  
         </nav>
     );
 };
@@ -34,21 +42,3 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, null)(Header);
-
-
-/*
-*                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home
-                                <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-* */
